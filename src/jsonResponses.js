be19625f2532c094b195a7a00320f3fe47b4236d
cnj3565 <cnj3565@g.rit.edu>
2022-09-19 23:38:35 -0400

    tried fixing http api assignment

---
+++ src/jsonResponses.js
@@ -0,0 +1,147 @@
+const respond = (request, response, status, content, type) => {
+  response.writeHead(status, { 'Content-Type': type });
+  response.write(content);
+  response.end();
+};
+
+// ---------------------------------------------------------------
+
+// Page Successfully Loaded
+const success = (request, response, type) => {
+  const responseMsg = {
+    message: 'This is a successful response',
+  };
+
+  if (type[0] === 'text/html') {
+    const xmlString = `<response>${responseMsg.message}</response>`;
+
+    return response(request, respond, 200, xmlString, 'text/xml');
+  }
+
+  const responseJSON = JSON.stringify(responseMsg);
+  return respond(request, response, 200, responseJSON, 'application/json');
+};
+
+// Bad Request Made (Valid True/Missing)
+const badRequest = (request, response, params, type) => {
+  const responseMsg = {
+    message: 'This request has the required parameters',
+  };
+  let status = 200;
+
+  if (!params.valid || params.valid !== 'true') {
+    responseMsg.message = 'Missing valid query param, set valid to true';
+    responseMsg.id = 'badRequestValidParam';
+    status = 400;
+  }
+
+  if (type[0] === 'text/html') {
+    const xmlString = `<response>${responseMsg.message}</response>`;
+
+    return response(request, respond, status, xmlString, 'text/xml');
+  }
+
+  const responseJSON = JSON.stringify(responseMsg);
+  return respond(request, response, status, responseJSON, 'application/json');
+};
+
+// Page Missing (404)
+const notFound = (request, response, type) => {
+  const responseMsg = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+
+  if (type[0] === 'text/html') {
+    const xmlString = `<response>${responseMsg.message}</response>`;
+
+    return respond(request, response, 404, xmlString, 'text/xml');
+  }
+
+  const responseJSON = JSON.stringify(responseMsg);
+  return respond(request, response, 404, responseJSON, 'application/json');
+};
+
+// Unauthorized Access (Loggin In/Logged Out)
+const unauthorized = (request, response, params, type) => {
+  const responseMsg = {
+    message: 'This request has the required parameters',
+  };
+  let status = 200;
+
+  if (!params.loggedIn || params.loggedIn !== 'true') {
+    responseMsg.message = 'You are not logged in, access is unauthorized';
+    responseMsg.id = 'unauthorizedAccessLoggedInParam';
+    status = 401;
+  }
+
+  if (type[0] === 'text/html') {
+    const xmlString = `<response>${responseMsg.message}</response>`;
+
+    return response(request, response, status, xmlString, 'text/xml');
+  }
+
+  const responseJSON = JSON.stringify(responseMsg);
+  return respond(request, response, status, responseJSON, 'application/json');
+};
+
+// Forbidden By Server
+const forbidden = (request, response, type) => {
+  const responseMsg = {
+    message: 'You don\'t have access to view this page on your server.',
+    id: 'forbidden',
+  };
+
+  if (type[0] === 'text/html') {
+    const xmlString = `<response>${responseMsg.message}</response>`;
+
+    return response(request, response, 403, xmlString, 'text/xml');
+  }
+
+  const responseJSON = JSON.stringify(responseMsg);
+  return respond(request, response, 403, responseJSON, 'application/json');
+};
+
+// Internal Server Error
+const serverError = (request, response, type) => {
+  const responseMsg = {
+    message: 'An Internal Server Error occured. Sorry about that.',
+    id: 'internalServerError',
+  };
+
+  if (type[0] === 'text/html') {
+    const xmlString = `<response>${responseMsg.message}</response>`;
+
+    return response(request, response, 500, xmlString, 'text/xml');
+  }
+
+  const responseJSON = JSON.stringify(responseMsg);
+  return respond(request, response, 500, responseJSON, 'application/json');
+};
+
+// Not Yet Implemented
+const notImplemented = (request, response, type) => {
+  const responseMsg = {
+    message: 'This hass\'t been implemented yet. Check back later.',
+    id: 'notImplemented',
+  };
+
+  if (type[0] === 'text/html') {
+    const xmlString = `<response>${responseMsg.message}</response>`;
+
+    return response(request, response, 500, xmlString, 'text/xml');
+  }
+
+  const responseJSON = JSON.stringify(responseMsg);
+  return respond(request, response, 500, responseJSON, 'application/json');
+};
+
+module.exports = {
+  success,
+  badRequest,
+  notFound,
+  unauthorized,
+  forbidden,
+  serverError,
+  notImplemented,
+};
---
+++ src/server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const url = require('url');
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+const urlStruct = {
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCSS,
+  '/success': jsonHandler.success, // 200 Code
+  '/badRequest': jsonHandler.badRequest, // Both 400 and 200 Code
+  notFound: jsonHandler.notFound, // 404 Code
+  '/unauthorized': jsonHandler.unauthorized, // Both 401 and 200 Code
+  '/forbidden': jsonHandler.forbidden, // 403 Code
+  '/internal': jsonHandler.serverError, // 500 Code
+  '/notImplemented': jsonHandler.notImplemented, // 401 Code
+};
+
+const onRequest = (request, response) => {
+  const parsedUrl = url.parse(request.url);
+  const queryParams = query.parse(parsedUrl.query);
+
+  if (urlStruct[parsedUrl.pathname]) {
+    urlStruct[parsedUrl.pathname](request, response, queryParams);
+  } else {
+    urlStruct.notFound(request, response);
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
